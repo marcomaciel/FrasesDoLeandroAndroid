696f56b60d6a421f47626847371c7742494cd5d8
Marco Antonio de Azevedo Maciel <marco@mmaciel.com.br>
2012-04-03 18:40:50 -0300

    first commit

---
+++ src/com/frasesdoleandro/FrasesDoLeandroAndroidActivity.java
@@ -0,0 +1,13 @@
+package com.frasesdoleandro;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FrasesDoLeandroAndroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
