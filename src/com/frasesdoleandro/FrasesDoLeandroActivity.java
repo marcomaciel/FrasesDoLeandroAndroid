39aaa16046da040f7b47ded7aec1d775cfa04862
Marco Antonio de Azevedo Maciel <marco@mmaciel.com.br>
2012-05-26 23:04:21 -0300

    Exclusao da classe de Controle. Inclusao das imagens de icone e aspas. Outrso pequenos ajustes

--- src/com/frasesdoleandro/FrasesDoLeandroActivity.java
+++ src/com/frasesdoleandro/FrasesDoLeandroActivity.java
@@ -17,7 +17,6 @@ public class FrasesDoLeandroActivity extends Activity {
 	TextView txtFrase;
 	Button btnOutraFrase;
 	String textoFrase;
-	
 	List<Frase> frases = new ArrayList<Frase>();
 	
 	/** Called when the activity is first created. */
@@ -25,45 +24,51 @@ public class FrasesDoLeandroActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-
+		
 		txtFrase = (TextView) findViewById(R.id.txtFrase);
+		try {
+			txtFrase.setText(selecionarFraseAleatoria());
+			System.out.println(selecionarFraseAleatoria());
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+
 		btnOutraFrase = (Button) findViewById(R.id.btnOutraFrase);
 		btnOutraFrase.setOnClickListener(new View.OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				try {
-					//FrasesController fc = new FrasesController();
-					textoFrase = selecionaFraseAleatoria();
+					textoFrase = selecionarFraseAleatoria();
 				} catch (Throwable e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 				txtFrase.setText(textoFrase);
-
 			}
 		});
 	}
 	
-	public String selecionaFraseAleatoria() throws IOException {
-		
-		Scanner leitor = new Scanner(getResources().openRawResource(R.raw.frases));
-
-		String linha = leitor.nextLine();
-		
-		while (leitor.hasNext()) {
-			System.out.println(linha);
-			linha = leitor.nextLine();
-			frases.add(new Frase(linha));
+	public String selecionarFraseAleatoria() throws IOException {
+		if (frases.isEmpty()){
+			frases = carregarFrases();
 		}
 		
 		Random random = new Random();
 		int numDaEscolha = random.nextInt(frases.size());
-		System.out.println("Frase escolhida: "
-				+ frases.get(numDaEscolha).toString() + " " + numDaEscolha);
-
+		//System.out.println("Frase escolhida: "+ frases.get(numDaEscolha).toString() + " " + numDaEscolha);
 		return frases.get(numDaEscolha).getTexto();
 	}
-
-
+	
+	public List<Frase> carregarFrases(){
+		List<Frase> frases = new ArrayList<Frase>();
+		
+		Scanner leitor = new Scanner(getResources().openRawResource(R.raw.frases));
+		String linha = null; //= leitor.nextLine();
+		
+		while (leitor.hasNext()) {
+			linha = leitor.nextLine();
+			frases.add(new Frase(linha));
+		}
+		return frases;
+	}
 }
